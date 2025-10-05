638a3cc77835a526ee3e27addb600864907454e1
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-10-05 13:19:12 +0200

    prepare 1.21.9 port

---
+++ 1.21.9/Common/src/main/java/fuzs/skeletonaifix/init/ModRegistry.java
@@ -0,0 +1,16 @@
+package fuzs.skeletonaifix.init;
+
+import fuzs.puzzleslib.api.init.v3.tags.TagFactory;
+import fuzs.skeletonaifix.SkeletonAIFix;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EntityType;
+
+public class ModRegistry {
+    static final TagFactory TAGS = TagFactory.make(SkeletonAIFix.MOD_ID);
+    public static final TagKey<EntityType<?>> WELL_BEHAVED_SKELETONS_ENTITY_TYPE_TAG = TAGS.registerEntityTypeTag(
+            "well_behaved_skeletons");
+
+    public static void bootstrap() {
+        // NO-OP
+    }
+}
---
+++ 1.21.9/NeoForge/src/main/java/fuzs/skeletonaifix/neoforge/SkeletonAIFixNeoForge.java
@@ -0,0 +1,16 @@
+package fuzs.skeletonaifix.neoforge;
+
+import fuzs.puzzleslib.api.core.v1.ModConstructor;
+import fuzs.puzzleslib.neoforge.api.data.v2.core.DataProviderHelper;
+import fuzs.skeletonaifix.SkeletonAIFix;
+import fuzs.skeletonaifix.data.ModEntityTypeTagProvider;
+import net.neoforged.fml.common.Mod;
+
+@Mod(SkeletonAIFix.MOD_ID)
+public class SkeletonAIFixNeoForge {
+
+    public SkeletonAIFixNeoForge() {
+        ModConstructor.construct(SkeletonAIFix.MOD_ID, SkeletonAIFix::new);
+        DataProviderHelper.registerDataProviders(SkeletonAIFix.MOD_ID, ModEntityTypeTagProvider::new);
+    }
+}
